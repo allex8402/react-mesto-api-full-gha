2bf18c7445dfb3a37a54110b072989b30858cb2e
allex8402 <120774992+allex8402@users.noreply.github.com>
2023-09-08 09:04:22 +0800

    15

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -6,7 +6,7 @@ function handleResponse(res) {
     return Promise.reject(res.status);
 }
 // базовый URL
-export const BASE_URL = "https://api.allexkate.nomoredomainsicu.ru";
+export const BASE_URL = "https://mesto.nomoreparties.co/v1/cohort-66";
 
 // регистрация в сервисе
 export function registerUser(email, password) {
