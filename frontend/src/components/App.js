31d58ecd91c670a881e9da58d1e25ce52b568823
allex8402 <120774992+allex8402@users.noreply.github.com>
2023-09-09 10:19:29 +0800

    35

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -73,13 +73,13 @@ function App() {
   }
 
   // Обработчик лайка карточки
-  function handleCardLike(card, isLiked) {
+  function handleCardLike(card) {
     // Снова проверяем, есть ли уже лайк на этой карточке
-    // const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((i) => i === currentUser._id);
 
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api
-      .changeLikeCardStatus(card._id, isLiked)
+      .changeLikeCardStatus(card._id, !isLiked)
       .then((newCard) => {
         setCards((state) => state.map((c) => (c._id === card._id ? newCard : c)));
       })
